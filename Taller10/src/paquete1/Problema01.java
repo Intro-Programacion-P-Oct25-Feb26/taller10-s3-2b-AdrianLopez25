0ee4cd848d04bf437854472cae6bf3beffcf6658
Adrian Lopez <israelruiz2874@gmail.com>
2025-12-18 10:36:41 -0500

    Problema01

--- Taller10/src/paquete1/Problema01.java
+++ Taller10/src/paquete1/Problema01.java
@@ -15,6 +15,37 @@ public class Problema01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        boolean[][] asistencia = {
+            {true, true, false, true, true},   
+            {true, false, false, true, false}, 
+            {true, true, true, true, true},    
+            {false, true, true, false, true}   
+        };
+
+        String[] estudiantes = {"Ana", "Luis", "Carlos", "María"};
+
+        int[] total_asistencias = new int[estudiantes.length];
+
+        for (int i = 0; i < total_asistencias.length; i++) {
+            int contador = 0;
+
+            for (int j = 0; j < asistencia[i].length; j++) {
+                if (asistencia[i][j] == true) {
+                    contador++;
+                }
+            }
+
+            total_asistencias[i] = contador;
+
+            System.out.printf("Estudiante:%s | Asistencias:%s "
+                    ,estudiantes[i],total_asistencias[i]);
+            
+            if (total_asistencias[i] == 5) {
+                System.out.println("Cumple con sus asistencias semanal");
+            } else {
+                System.out.println("No cumple con todas sus asistencias");
+            }
+        }
     }
-    
 }
+    
