190f6db4570352e6dc58c7887f6483c51644637e
Adrian Lopez <israelruiz2874@gmail.com>
2025-12-18 11:32:03 -0500

    Problema03

--- Taller10/src/paquete3/Problema03.java
+++ Taller10/src/paquete3/Problema03.java
@@ -10,5 +10,43 @@ package paquete3;
  * @author reroes
  */
 public class Problema03 {
-    
+    public static void main(String[] args) {
+        boolean[][] sensores = {
+            {true, true, true, true, true, true},   // Zona 0
+            {true, false, true, false, true, false}, // Zona 1
+            {false, false, false, false, false, false}, // Zona 2
+            {true, true, false, true, true, false} // Zona 3
+        };
+
+        int sin_fallos = 0;
+        String reporte = "";
+
+        for (int i = 0; i < sensores.length; i++) {
+            int fallos = 0;
+
+            for (int j = 0; j < sensores[i].length; j++) {
+                if (sensores[i][j] == false) {
+                    fallos = fallos + 1;
+                }
+            }
+
+            if (fallos == 0) {
+                sin_fallos=  sin_fallos + 1;
+            } else {
+                reporte = String.format(
+                        "%sZona %d presenta %d fallos\n",
+                        reporte,
+                        i ,
+                        fallos);
+            }
+        }
+
+        reporte = String.format(
+                "%s\nZonas que funcionan sin fallos: %d",
+                reporte,
+                sin_fallos
+        );
+
+        System.out.println(reporte);
+    }
 }
