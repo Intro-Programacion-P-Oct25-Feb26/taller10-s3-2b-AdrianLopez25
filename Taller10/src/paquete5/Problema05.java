4495b397dd325fb6687a3ecdf346b1400eec8eb6
Adrian Lopez <israelruiz2874@gmail.com>
2025-12-18 12:19:09 -0500

    Problema05

--- Taller10/src/paquete5/Problema05.java
+++ Taller10/src/paquete5/Problema05.java
@@ -10,5 +10,32 @@ package paquete5;
  * @author reroes
  */
 public class Problema05 {
-    
+    public static void main(String[] args) {
+
+        String[][] estudiantes = {
+            {"Sarah Ward", "Philip Payne"},
+            {"Carrie Burton", "Lauren Rice"},
+            {"Emma Escobar", "Lori Flores"},
+            {"Steven West", "Toni Martin"}
+        };
+
+        String acumulador = "";
+
+        for (int i = 0; i < estudiantes.length; i++) {
+            for (int j = 0; j < estudiantes[i].length; j++) {
+                
+                String nombre = estudiantes[i][j];
+
+                char letra = nombre.charAt(0);
+
+                if (letra == 'S' || letra == 'P' || letra == 'T') {
+                    acumulador = acumulador + nombre + "\n";
+                }
+            }
+        }
+
+        System.out.println("Estudiantes con las primeras letras (S, P, T)");
+        System.out.println(acumulador);
+    }
 }
+
