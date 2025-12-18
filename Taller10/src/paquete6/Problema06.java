1de0a4bbc76da22745e3234c37b48800e2c34df7
Adrian Lopez <israelruiz2874@gmail.com>
2025-12-18 12:33:37 -0500

    Problema06

--- Taller10/src/paquete6/Problema06.java
+++ Taller10/src/paquete6/Problema06.java
@@ -4,11 +4,52 @@
  * and open the template in the editor.
  */
 package paquete6;
-
+import java.util.Scanner;
 /**
  *
  * @author reroes
  */
 public class Problema06 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
 
+        double[][] matriz = new double[5][6];
+        double[][] matriz_resultante = new double[5][6];
+        double multiplicador;
+
+        System.out.print("Ingrese un número para multiplicar: ");
+        multiplicador = entrada.nextDouble();
+
+        System.out.println("\n INGRESO DE DATOS:");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 6; j++) {
+                System.out.print("Fila " + i + ", Columna " + j + ": ");
+                matriz[i][j] = entrada.nextDouble();
+            }
+        }
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 6; j++) {
+                matriz_resultante[i][j] = matriz[i][j] * multiplicador;
+            }
+        }
+
+        System.out.println("\n MATRIZ INICIAL :");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 6; j++) {
+                System.out.print(matriz[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n MATRIZ RESULTANTE (Multiplicada por : " + multiplicador + ")");
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 6; j++) {
+                System.out.print(matriz_resultante[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
 }
+
+
