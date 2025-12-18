30cdb83901227f9e724de214d8a8b17399699789
Adrian Lopez <israelruiz2874@gmail.com>
2025-12-18 10:58:31 -0500

    Problema02

--- Taller10/src/paquete2/Problema02.java
+++ Taller10/src/paquete2/Problema02.java
@@ -10,5 +10,42 @@ package paquete2;
  * @author reroes
  */
 public class Problema02 {
-    
+    public static void main(String[] args) {
+
+        int[][] edades = {
+            {5, 12, 35, 40},  
+            {8, 15, 33, 38},  
+            {6, 10, 30, 36}   
+        };
+
+        int total_menores = 0;
+        int[] adultos_familia = new int[edades.length];
+
+        for (int i = 0; i < edades.length; i++) {
+            int contador_a = 0; 
+
+            for (int j = 0; j < edades[i].length; j++) {
+                int edad1 = edades[i][j];
+
+                if (edad1 < 18) {
+                    total_menores++;
+                } else {
+                    contador_a++;
+                }
+            }
+
+            adultos_familia[i] = contador_a;
+        }
+
+        System.out.printf("Total de menores de edad: "
+                + total_menores);
+        
+        System.out.println("\nTotal de adultos por familia:");
+        for (int i = 0; i < adultos_familia.length; i++) {
+            System.out.println("Familia " + (i+1) + ": " + adultos_familia[i]
+                    + " adultos");
+        }
+    }
 }
+   
+
